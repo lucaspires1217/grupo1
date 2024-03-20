fe4777eedbed9b08c6172095d6836d4db5e1eadc
brendka <161617088+brendka@users.noreply.github.com>
2024-03-20 08:52:03 -0300

    aula 05

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelector(".contador");
+const tempoObjetivo1 = new Date ("2024-10-05T00-:00:00")
+contadores[0]
+contadores[0].textContent= tempoObjetivo1;
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
