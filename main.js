0194ee113bf14729f6a07dc1273d6a33f799088f
brendka <161617088+brendka@users.noreply.github.com>
2024-03-27 08:53:58 -0300

    aula6

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelector(".contador");
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2024-10-05T00-:00:00");
 contadores[0]
 contadores[0].textContent= tempoObjetivo1 - tempoAtual;
@@ -19,9 +19,14 @@ for (let i = 0; i < botoes.length; i++)
 function calculadoraTempo(tempoObjetivo){
     let tempoAtual = new date();
     let tempofinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempofina / 1000);
+    let segundos = Math.floor(tempofinal / 1000);
     let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(dias/24);
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
 
-    return segundos;
+    return dias + " didas "+ horas + " horas " + minutos + " minutos " + segundos + "segundos";
 }
 
