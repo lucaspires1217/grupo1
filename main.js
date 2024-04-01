658c7d90b8d8620ac84500221c717e63070c5c8f
brendka <161617088+brendka@users.noreply.github.com>
2024-04-01 08:52:00 -0300

    aula 7

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2024-10-05T00-:00:00");
+const tempoObjetivo2 = new Date ("2024-10-05T00-:00:00");
+const tempoObjetivo3 = new Date ("2024-10-05T00-:00:00");
+const tempoObjetivo4 = new Date ("2024-10-05T00-:00:00");
+const tempo=[tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 contadores[0]
-contadores[0].textContent= tempoObjetivo1 - tempoAtual;
+contadores[0]. textContent = caucula (tempo[0]);
 
 for (let i = 0; i < botoes.length; i++)
     botoes[i].onclick = function () {
@@ -27,6 +32,11 @@ function calculadoraTempo(tempoObjetivo){
     minutos%=60;
     horas%=24;
 
-    return dias + " didas "+ horas + " horas " + minutos + " minutos " + segundos + "segundos";
+    return dias + " dias "+ horas + " horas " + minutos + " minutos " + segundos + "segundos";
 }
-
+for (let i=0; i< contadores.length; i++){
+    contadores[i]. textContent = caucula (tempo[i]);
+    contadores[i].textContent= calculadoraTempo(tempo[i]);
+}
+atualizaCronometro(i);
+setInterval(atualizaCronometro, 1000);
